76d0502684f17dc7324258bb9f916b8973cba3a9
eccotw8952 <pajiaduh2222@gmail.com>
2022-06-08 03:51:34 +0500

    New component Card with scss _card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 
+import { GrFormAdd } from 'react-icons/gr';
+
 import sneaker from '../assets/img/sneakers/air-force.jpg';
 
 function Card() {
   return (
     <div className='product-card'>
-      <img src={sneaker} alt='' />
+      <img src={sneaker} alt='' className='product-card__img' />
       <div className='product-card__body'>
+        'Nike'
         <div className='product-card__title'>
-          'Nike'
           <span>Sneaker</span>
           <span>Air Force 1 High Crater</span>
         </div>
-        <div className='product-card__price'></div>
+        <div className='product-card__price'>
+          <span>17005 p.</span>
+          <button className='btn-border'>
+            <GrFormAdd size={15} />
+          </button>
+        </div>
       </div>
     </div>
   );
