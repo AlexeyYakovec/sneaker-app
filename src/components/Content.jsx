2e78226a0827dcf58be64b1be5caefbc5caa05bb
eccotw8952 <pajiaduh2222@gmail.com>
2022-06-08 19:45:33 +0500

    work with props

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,51 @@
 import React from 'react';
 import Card from './Card';
 
+const data = [
+  {
+    img: './assets/img/sneakers/air-force.jpg',
+    brand: 'Nike',
+    shoeType: 'sneaker',
+    title: 'Air Force 1 High Crater',
+    price: '17 490',
+  },
+  {
+    img: './assets/img/sneakers/air-mada.jpg',
+    brand: 'Nike',
+    shoeType: 'sneaker',
+    title: 'ACG Air Mada',
+    price: '13 690',
+  },
+  {
+    img: './assets/img/sneakers/air-mowabb.jpg',
+    brand: 'Nike',
+    shoeType: 'sneaker',
+    title: 'ACG Air Mowabb',
+    price: '15 690',
+  },
+  {
+    img: './assets/img/sneakers/jogger.jpg',
+    brand: 'Adidas original',
+    shoeType: 'sneaker',
+    title: 'Nite Jogger Winterized',
+    price: '14 990',
+  },
+  {
+    img: './assets/img/sneakers/ozrah.jpg',
+    brand: 'Adidas original',
+    shoeType: 'sneaker',
+    title: 'Ozrah',
+    price: '12 790',
+  },
+  {
+    img: './assets/img/sneakers/ozzelia.jpg',
+    brand: 'Adidas original',
+    shoeType: 'sneaker',
+    title: 'Ozelia',
+    price: '10 490',
+  },
+];
+
 function Content() {
   return (
     <div className='content'>
@@ -10,7 +55,10 @@ function Content() {
       </div>
 
       <div className='product-contenct'>
-        <Card />
+        {data.map(({}) => (
+          <Card />
+        ))}
+
         <Card />
         <Card />
       </div>
